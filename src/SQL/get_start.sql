4c5ea2cc88ab233723fa7f28b76fea3e08bc8c6e
Joseph-Essa <josepheissa0@gmail.com>
2025-06-20 21:36:44 +0300

    craete get start

---
+++ src/SQL/get_start.sql
@@ -0,0 +1,18 @@
+select (FIRST_NAME) from EMPLOYEES;
+
+SELECT EMPLOYEE_ID , FIRST_NAME || ' is son of ' || LAST_NAME || ' takes salary = ' || SALARY as "Emp report" from EMPLOYEES ;
+
+SELECT EMPLOYEE_ID , FIRST_NAME ||' '||LAST_NAME as "Full Name" , SALARY , SALARY*12 as "Annual SALARY " , 
+(SALARY+100)*12/5 as "Bouns per Year" 
+from EMPLOYEES WHERE  JOB_ID = 'SA_REP'
+order by HIRE_DATE desc; 
+
+
+SELECT JOB_ID , COUNT(*) as "Num Of Emp Per job" FROM EMPLOYEES GROUP BY JOB_ID; 
+
+select *from EMPLOYEES where LAST_NAME like '%s%' ;
+select * from EMPLOYEES WHERE lower(FIRST_NAME) LIKE lower('%e_') ;
+
+SELECT * from EMPLOYEES WHERE DEPARTMENT_ID is NULL ;
+
+
